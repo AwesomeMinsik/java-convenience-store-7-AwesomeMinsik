d914df1f29b23780774726a7668a703f46e83602
AwesomeMinsik <beaut3579@gmail.com>
2024-11-07 16:10:13 +0900

    refactor : 상품 클래스의 불필요한 setter 메서드 를 제거하였습니다

--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -8,7 +8,7 @@ public class Product {
     private String name;
     private int price;
     private int quantity;
-    private PromotionPolicy promotion;
+    private final PromotionPolicy promotion;
 
     public Product(String name, int price, int quantity, PromotionPolicy promotion) {
         this.name = name;
@@ -21,24 +21,18 @@ public class Product {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getPrice() {
         return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
     }
 
     public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public void setQuantity(int orderedQuantity ) {
+        if (quantity > orderedQuantity)
+            quantity -= orderedQuantity;
+        // TODO: 재고 부족시 메시지 호출 어디서할지 정하기
     }
 
     public PromotionPolicy getPromotion() {
