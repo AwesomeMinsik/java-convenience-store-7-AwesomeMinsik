474b47e4fa8396d64ebf71ac53f9753d2b82e15f
AwesomeMinsik <beaut3579@gmail.com>
2024-11-10 19:02:23 +0900

    feat : 멤버십 할인, 프로모션 증정 여부를 입력받는 기능을 추가하였습니다

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -4,17 +4,26 @@ import camp.nextstep.edu.missionutils.Console;
 import store.util.parser.InputParser;
 import store.util.validate.StringValidator;
 
-import java.util.List;
 import java.util.Map;
 
 public abstract class InputView {
-    public static List<Map<String, Integer>> inputOrder() {
+    public static Map<String, Integer> inputOrder() {
+        System.out.println();
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예 : [사이다-2],[감자칩-1])");
         return InputParser.parseOrder(Console.readLine());
     }
-    public static String applyMembershipDiscount(){
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
-        String MembershipDiscount = Console.readLine();
-        StringValidator.validateMembershipDiscountInput(MembershipDiscount);
-        return MembershipDiscount;
+
+    public static String applyMembershipDiscount() {
+        String input;
+        while (true){
+            try {
+                input =Console.readLine();
+                StringValidator.validateMembershipDiscountInput(input);
+                break;
+            }catch (IllegalArgumentException e){
+                System.out.println("[ERROR] 잘못된 입력입니다 다시 입력해주세요.");
+            }
+        }
+            return input;
     }
 }
