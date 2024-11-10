5ed9a57fd5ae77e097658a64082403ac2f9e90b8
AwesomeMinsik <beaut3579@gmail.com>
2024-11-10 20:54:30 +0900

    refactor : 구매상품과 프로모션 조건을 비교하는 메서드의 역할을 분리하였습니다
    - 메서드를 추가하여 프로모션 증정 여부에 대한 사용자입력을 받아 분기처리하는 역할을 분리하였습니다

--- src/main/java/store/model/ProductManager.java
+++ src/main/java/store/model/ProductManager.java
@@ -1,11 +1,9 @@
 package store.model;
 
-import store.factory.OrderItemFactory;
+import store.util.validate.ProductValidater;
 import store.view.InputView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public abstract class ProductManager {
@@ -35,18 +33,17 @@ public abstract class ProductManager {
         int freeItem;
         boolean isYes=false;
         freeItem = orderedProduct.getQuantity() / product.getPromotion().getBuy();
+        isYes = isYes(product, orderedProduct, freeItem, isYes);
+        applyPromotionGift(product, orderedProduct, isYes, freeItem);
+    }
+
+    private static boolean isYes(Product product, OrderedProduct orderedProduct, int freeItem, boolean isYes) {
         if (freeItem > 0) {
             System.out.println("현재 " + product.getName() + "은(는) " + freeItem + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
             isYes = inputIsYes(product, orderedProduct);
 
         }
-        if (isYes) {
-            System.out.println(product);
-            System.out.println("orderedProduct = " + orderedProduct);
-            System.out.println("freeItem = " + freeItem);
-            product.deductStockByQuantity(orderedProduct.getQuantity() + freeItem);
-            System.out.println(product);
-        } else product.deductStockByQuantity(orderedProduct.getQuantity());
+        return isYes;
     }
 
     public static boolean inputIsYes(Product product, OrderedProduct orderedProduct) {
@@ -55,5 +52,15 @@ public abstract class ProductManager {
             return input.equalsIgnoreCase("y");
         }
         return false;
+    }
+
+    private static void applyPromotionGift(Product product, OrderedProduct orderedProduct, boolean isYes, int freeItem) {
+        if (isYes) {
+            System.out.println(product);
+            System.out.println("orderedProduct = " + orderedProduct);
+            System.out.println("freeItem = " + freeItem);
+            product.deductStockByQuantity(orderedProduct.getQuantity() + freeItem);
+            System.out.println(product);
+        } else product.deductStockByQuantity(orderedProduct.getQuantity());
     }
 }
