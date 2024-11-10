d8c012bf76bf24d9c4050d85d9faa420343c5324
AwesomeMinsik <beaut3579@gmail.com>
2024-11-10 18:52:37 +0900

    feat : 문자열 검증 클래스를 추가하였습니다

--- src/main/java/store/util/validate/StringValidator.java
+++ src/main/java/store/util/validate/StringValidator.java
@@ -9,4 +9,20 @@ public abstract class StringValidator {
             throw new IllegalArgumentException();
         }
     }
+
+    public static void validateMembershipDiscountInput(String membershipDiscount) {
+        validateNull(membershipDiscount);
+        validateInput(membershipDiscount);
+    }
+
+    public static void validateInput(String membershipDiscount) {
+        if (!"y".equalsIgnoreCase(membershipDiscount) && !"n".equalsIgnoreCase(membershipDiscount)) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 'y' 또는 'n'만 입력 가능합니다.");
+        }
+    }
+
+    public static void validateNull(String membershipDiscount){
+        if (membershipDiscount.isEmpty())
+            throw new IllegalArgumentException("잘못된 입력입니다. 'y' 또는 'n'만 입력 가능합니다.");
+    }
 }
