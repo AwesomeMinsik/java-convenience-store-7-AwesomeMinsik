313f6dddf2f30f549344f95b3990a150ba2a7c5a
AwesomeMinsik <beaut3579@gmail.com>
2024-11-10 19:13:16 +0900

    refactor : 상품목록 컬렉션 구조 변경에 따른 출력 방식 및 반환값을 업데이트 하였습니다

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -2,23 +2,24 @@ package store.view;
 
 import store.model.Product;
 
-import java.util.List;
+import java.util.Map;
 
 public abstract class OutputView {
-    public static List<Product> printProductList(List<Product> productList){
+    public static Map<Integer, Product> printProductList(Map<Integer, Product> productList) {
         System.out.println("안녕하세요. W편의점입니다.");
-        System.out.println("현재 보유하고 있는 상품입니다");
+        System.out.println("현재 보유하고 있는 상품입니다.");
         System.out.println();
-        productList.forEach(System.out::println);
-        System.out.println();
-        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예 : [사이다-2],[감자칩-1])");
+
+        productList.values().forEach(System.out::println);
         return productList;
     }
-    public static void printBillLetter(){
+
+    public static void printBillLetter() {
         System.out.println("==============W 편의점================");
         System.out.println("상품명\t\t수량\t금액");
     }
+
     public static void printProductStatus(Product product) {
-        System.out.println(product.getName() + "의 남은 수량"+product.getQuantity()+"개");
+        System.out.println("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
     }
 }
