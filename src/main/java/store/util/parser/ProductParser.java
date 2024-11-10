5376d5196490a28af66ab1e07ffec4bb0d933f5d
AwesomeMinsik <beaut3579@gmail.com>
2024-11-10 19:23:25 +0900

    refactor: 상품목록 컬렉션 구조 변경에 따른 상태를 업데이트 하였습니다

--- src/main/java/store/util/parser/ProductParser.java
+++ src/main/java/store/util/parser/ProductParser.java
@@ -1,63 +1,44 @@
 package store.util.parser;
 
 import store.factory.ProductFactory;
+import store.factory.PromotionFactory;
 import store.model.Product;
 import store.policy.PromotionPolicy;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-import static store.policy.PromotionPolicy.*;
+import static store.util.files.PromotionLoader.getPromotionList;
 
 public abstract class ProductParser {
-    public static List<Product> getProductList(List<String> parts) {
-        List<Product> products = new ArrayList<>();
+    public static Map<Integer, Product> getProductList(List<String> parts) {
+
+        Map<Integer, Product> products = new LinkedHashMap<>();
+        int key=0;
         for (String line : parts) {
-            parseListToProduct(line, products);
+            key++;
+            parseListToProduct(line, products,key);
         }
         return products;
     }
 
-    private static void parseListToProduct(String line, List<Product> products) {
+    private static void parseListToProduct(String line, Map<Integer, Product> products, int key) {
         List<String> parts = Arrays.stream(line.split(","))
                 .map(String::trim)
                 .toList();
-        products.add(ProductFactory.createProductObject(parts));
-    }
-    public static PromotionPolicy fromString(String promotion) {
-        if (promotion.equalsIgnoreCase("null")) {
-            return NONE;
-        }
-        return switch (promotion) {
-            case "MD추천상품" -> MD_RECOMMENDED;
-            case "탄산2+1" -> BUY2GET1;
-            case "반짝할인" -> LIMITED_DISCOUNT;
-            default -> NONE;
-        };
+        products.put(key,ProductFactory.createProductObject(parts, getPromotionList()));
     }
 
-    public static List<String> parseProductToString(List<Product> products) {
-        List<String> productStrings = new ArrayList<>();
-        for (Product product : products) {
-            productStrings.add(String.join(",",
-                            product.getName(),
-                            String.valueOf(product.getPrice()),
-                            String.valueOf(product.getQuantity()),
-                            toString(product.getPromotion())));
+
+    public static PromotionPolicy getPromotionType(String promotion, List<PromotionPolicy> promotionPolicies) {
+        for (PromotionPolicy promotionPolicy : promotionPolicies) {
+            if (promotionPolicy.getName().equalsIgnoreCase(promotion))
+                return promotionPolicy;
         }
-        return productStrings;
+        return null;
     }
 
-    public static String toString(PromotionPolicy promotionPolicy) {
-        if (promotionPolicy == NONE) {
-            return "null";
-        }
-        return switch (promotionPolicy) {
-            case MD_RECOMMENDED -> "MD추천상품";
-            case BUY2GET1 -> "탄산2+1";
-            case LIMITED_DISCOUNT -> "반짝할인";
-            default -> "null";
-        };
+    public static PromotionPolicy parseStringToPromotionType(String line) {
+        String[] split = line.split(",");
+        return PromotionFactory.createPromotion(split[0], Integer.parseInt(split[1]), Integer.parseInt(split[2]), split[0], split[0]);
     }
 }
