e1cd7d1cc896c59aa35231cd8b809939a0df3779
AwesomeMinsik <beaut3579@gmail.com>
2024-11-10 19:12:21 +0900

    refactor : 프로모션 클래스 구조 변경에 따른 ProductFactory 클래스의 상품 객체 생성 로직을 변경하였습니다

--- src/main/java/store/factory/ProductFactory.java
+++ src/main/java/store/factory/ProductFactory.java
@@ -1,16 +1,17 @@
 package store.factory;
 
 import store.model.Product;
+import store.policy.PromotionPolicy;
+import store.util.parser.ProductParser;
 
 import java.util.List;
 
-import static store.parser.ProductParser.fromString;
 
 public abstract class ProductFactory {
-    public static Product createProductObject(List<String> parts){
+    public static Product createProductObject(List<String> parts, List<PromotionPolicy> promotionList){
         return new Product(parts.get(0),
                 Integer.parseInt(parts.get(1)),
                 Integer.parseInt(parts.get(2)),
-                fromString(parts.get(3)));
+                ProductParser.getPromotionType(parts.get(3),promotionList));
     }
 }
