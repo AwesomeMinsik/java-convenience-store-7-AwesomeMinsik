05099d1bb67b8bd934c9f972a9bce131ba2cd453
AwesomeMinsik <beaut3579@gmail.com>
2024-11-10 20:52:52 +0900

    feat : 입력 수량과 상품목록을 비교하여 재고 부족 메시지를 출력하는 기능을 추가하였습니다

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,29 +1,53 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.model.Product;
+import store.model.ProductManager;
 import store.util.parser.InputParser;
+import store.util.validate.ProductValidater;
 import store.util.validate.StringValidator;
 
 import java.util.Map;
 
 public abstract class InputView {
-    public static Map<String, Integer> inputOrder() {
+    public static Map<String, Integer> inputOrder(Map<Integer, Product> products) {
         System.out.println();
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예 : [사이다-2],[감자칩-1])");
-        return InputParser.parseOrder(Console.readLine());
+        Map<String, Integer> orderList;
+        while (true) {
+            try {
+                orderList = InputParser.parseOrder(Console.readLine());
+                isStockAvailable(products, orderList);
+                break;
+            }catch (IllegalArgumentException e){
+                System.out.println("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+            }
+        }
+        return orderList;
+    }
+
+    private static void isStockAvailable(Map<Integer, Product> products, Map<String, Integer> orderList) {
+        for (Map.Entry<Integer, Product> entry : products.entrySet()) {
+            Product product = entry.getValue();
+
+            if (orderList.containsKey(product.getName())) {
+                Integer quantity = orderList.get(product.getName());
+                ProductValidater.validateOrderQuantity(product, quantity);
+            }
+        }
     }
 
     public static String applyMembershipDiscount() {
         String input;
-        while (true){
+        while (true) {
             try {
-                input =Console.readLine();
+                input = Console.readLine();
                 StringValidator.validateMembershipDiscountInput(input);
                 break;
-            }catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] 잘못된 입력입니다 다시 입력해주세요.");
             }
         }
-            return input;
+        return input;
     }
 }
