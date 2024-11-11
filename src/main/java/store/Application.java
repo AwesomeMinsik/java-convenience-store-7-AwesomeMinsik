3d3ee8f3cbfcda0dbf3ab9edd09b1ca15b9ef070
AwesomeMinsik <beaut3579@gmail.com>
2024-11-11 19:28:54 +0900

    feat : 결제종료 후 사용자 입력에 따라 재구매 하는 기능을 추가하였습니다

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,10 +1,21 @@
 package store;
 
 import store.controller.StoreManager;
+import store.model.Product;
+import store.util.files.FileManager;
+import store.view.OutputView;
+
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        StoreManager.OrderResult orderResult = StoreManager.getOrder();
-        StoreManager.payments(orderResult.productByOrder(), orderResult.orderQuantities());
+        Map<Integer, Product> productList = FileManager.getProductList();
+        while (true) {
+            StoreManager.OrderResult orderResult = StoreManager.getOrder(productList);
+            StoreManager.payments(orderResult.productByOrder(), orderResult.orderQuantities());
+            String string = OutputView.askRepurchaseConfirmation();
+            if (string.equalsIgnoreCase("n"))
+                break;
+        }
     }
 }
