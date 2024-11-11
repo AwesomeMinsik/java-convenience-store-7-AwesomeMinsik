349f4e7e0d62595712d523be4d2a28efc85a87e3
AwesomeMinsik <beaut3579@gmail.com>
2024-11-11 21:07:28 +0900

    refactor : 영수증 클래스명을 변경하였습니다
    - 클래스명 오타 수정 및 역할에 맞는 메서드 명으로 변경하였습니다

--- src/main/java/store/controller/BillLetter.java
+++ src/main/java/store/controller/BillLetter.java
@@ -5,9 +5,9 @@ import store.view.OutputView;
 
 import java.util.Map;
 
-public abstract class VillLetter {
+public abstract class BillLetter {
     public static void getPaymentsResult(Map<String, OrderedProduct> orderedItem, int allTotal) {
-        OutputView.printBillLetter();
+        OutputView.printHeader();
         OutputView.printOrderList(orderedItem);
         OutputView.printResult(allTotal);
     }
