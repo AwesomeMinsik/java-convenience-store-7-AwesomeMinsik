da624ff1ee793fed8f4bc0990556408459f34be0
AwesomeMinsik <beaut3579@gmail.com>
2024-11-11 19:23:23 +0900

    feat : 프로모션, 일반상품을 구분하는 클래스를추가하였습니다
    - 상품의 프로모션 여부에 따라 증정품을 계산하는 역할을 담당합니다.

--- src/main/java/store/service/PromotionService.java
+++ src/main/java/store/service/PromotionService.java
@@ -2,23 +2,72 @@ package store.service;
 
 import store.model.OrderedProduct;
 import store.model.Product;
-import store.model.ProductManager;
+import store.view.OutputView;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+
 public abstract class PromotionService {
 
-    public static void apply(Map<Product,String> productByOrder, Map<String,OrderedProduct> orderedItem) {
+    private static Set<String> promotedProducts = new HashSet<>();
+
+    public static void apply(Map<Product, String> productByOrder, Map<String, OrderedProduct> orderedItem) {
+        int total = 0;
+        int nonPromotionalTotal = 0;
+        int freeItem = 0;
+        int saled = 0;
         for (Map.Entry<Product, String> entry : productByOrder.entrySet()) {
             Product product = entry.getKey();
             OrderedProduct orderedProduct = orderedItem.get(product.getName());
-            ProductManager.deductStockProduct(product, orderedProduct);
 
-            if (product.getPromotion().getName().isEmpty()) {
-                ProductManager.deductStockProduct(product, orderedProduct);
+            int orderedProductQuantity = orderedProduct.getQuantity();
+            int dealQuantity = 0;
+
+            if (!product.getPromotion().getName().isEmpty()) {
+                dealQuantity = getMaxPromotionQuantity(product);
             }
-            else
-                ProductManager.deductStockProduct(product, orderedProduct);
+
+            if (dealQuantity > 0) {
+                int realQuantity = Math.min(dealQuantity, orderedProductQuantity);
+                if (!product.getPromotion().getName().isEmpty()) {
+                    total = getTotalWithDeductStock(orderedProduct, product, realQuantity, total);
+                }
+                promotedProducts.add(product.getName());
+                orderedProductQuantity -= realQuantity;
+            }
+
+            if (orderedProductQuantity > 0 && !promotedProducts.contains(product.getName())) {
+                if (product.getPromotion().getName().isEmpty()) {
+                    product.deductStockByQuantity(orderedProductQuantity);
+                    nonPromotionalTotal += orderedProduct.getTotalPrice();
+                }
+            }
         }
 
+        int allTotal= total + nonPromotionalTotal;
+
+        OutputView.printOrderList(orderedItem);
+        OutputView.printResult(allTotal);
+    }
+
+    private static int getTotalWithDeductStock(OrderedProduct orderedProduct, Product product, int realQuantity, int total) {
+        int freeItem;
+        freeItem = orderedProduct.getQuantity() / product.getPromotion().getBuy();
+        if (orderedProduct.getQuantity() % (product.getPromotion().getBuy() + 1) == 0) {
+            product.deductStockByQuantity(realQuantity);
+            total += orderedProduct.getTotalPrice();
+        }
+        if (!(orderedProduct.getQuantity() % (product.getPromotion().getBuy() + 1) == 0)) {
+            product.deductStockByQuantity(realQuantity + freeItem);
+            orderedProduct.setQuantity(realQuantity + freeItem);
+            total += orderedProduct.getTotalPrice();
+        }
+        return total;
+    }
+
+    private static int getMaxPromotionQuantity(Product product) {
+        int maxPromotion = (product.getQuantity() / (product.getPromotion().getBuy() + 1)) * (product.getPromotion().getBuy() + 1);
+        return Math.min(maxPromotion, product.getQuantity());
     }
 }
